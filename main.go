c46b0d09ae811cef1a019d4b839a169e9c4a4679
yamasaki <yamasakipann0218@gmail.com>
2023-09-06 03:47:47 +0900

    feat : health apo

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+	router.Run()
 }
